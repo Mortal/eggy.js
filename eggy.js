a784962b4b37b182fb8e96abbe4dd53808a0140c
Mathias Rav <rav@cs.au.dk>
2011-03-28 18:13:49 +0200

    eggy.js: remove dumb event handlers for console log (already handled by enabled debugging)

--- eggy.js
+++ eggy.js
@@ -1,5 +1,4 @@
-var sys = require('sys'),
-    net = require('net'),
+var net = require('net'),
     irc = require('./irc'),
     Listener = require('./Listener').Listener;
 
@@ -9,30 +8,6 @@ var bot = new irc.Client('irc.eu.gamesurge.net', 'eggyjs', {
 });
 
 var listeners = [];
-
-bot.addListener('error', function(message) {
-  sys.puts('ERROR: ' + message.command + ': ' + message.args.join(' '));
-});
-
-bot.addListener('message#blah', function (from, message) {
-  sys.puts('<' + from + '> ' + message);
-});
-
-bot.addListener('message', function (from, to, message) {
-  sys.puts(from + ' => ' + to + ': ' + message);
-});
-bot.addListener('pm', function(nick, message) {
-  sys.puts('Got private message from ' + nick + ': ' + message);
-});
-bot.addListener('join', function(channel, who) {
-  sys.puts(who + ' has joined ' + channel);
-});
-bot.addListener('part', function(channel, who, reason) {
-  sys.puts(who + ' has left ' + channel + ': ' + reason);
-});
-bot.addListener('kick', function(channel, who, by, reason) {
-  sys.puts(who + ' was kicked from ' + channel + ' by ' + by + ': ' + reason);
-});
 
 var commandserver = net.createServer(function (socket) {
   var listener = new Listener(socket, bot);
