5cfa7b7ff6dccca3536a69d513c93de0569f008a
Mathias Rav <rav@cs.au.dk>
2011-03-28 18:42:39 +0200

    module minecraft.js: change to using lolmc library

--- modules/minecraft.js
+++ modules/minecraft.js
@@ -1,56 +1,32 @@
 var Module = require('./Module').Module,
-		net = require('net');
+		net = require('net'),
+		lolmc = require('lolmc');
 var m = new Module();
-var mc = net.createConnection('/tmp/minecraft');
-var swallowlines = {};
-function ondata(data) {
-	var lines = data.toString().split('\n');
-	for (var i = 0, l = lines.length; i < l; ++i) {
-		var line = lines[i];
-		var o = line.match(/^20\d\d-..-.. ..:..:.. \[INFO\] (.*)/);
-		if (!o) return;
-		line = o[1];
-		o = line.match(/^<[^>]*> .*/);
-		if (o) {
-			m.say('#concerned', line.replace(/Alakala/gi, 'kala'));
-			return;
-		}
-		o = line.match(/^\[CONSOLE\] (.*)/);
-		if (o) {
-			var msg = o[1];
-			if (swallowlines[msg]) {
-				var latency = new Date().getTime() - swallowlines[msg].getTime();
-				if (latency > 2000) {
-					console.log("Latency to minecraft server: "+latency+"ms");
-				}
-				delete swallowlines[msg];
-			} else {
-				m.say('#concerned', line.replace(/Alakala/g, 'kala'));
-			}
-			return;
-		}
-		o = line.match(/^([a-zA-Z0-9]+) .* logged in with entity id/);
-		if (o) {
-			m.say('#concerned', o[1]+' is now playing Minecraft');
-			return;
-		}
-		o = line.match(/^([a-zA-Z0-9]+) lost connection: (.*)/);
-		if (o) {
-			m.say('#concerned', o[1]+' quit Minecraft: '+o[2]);
-			return;
-		}
+var mc = new lolmc.Minecraft();
+mc.on('message', function (sender, message) {
+	var line = sender+": "+message;
+	m.say('#concerned', line.replace(/Alakala/gi, 'kala'));
+});
+mc.on('consolemessage', function (message) {
+	var line = "[Console] "+message;
+	m.say('#concerned', line.replace(/Alakala/gi, 'kala'));
+});
+mc.on('latency', function (latency) {
+	if (latency > 2000) {
+		console.log("Latency to minecraft server: "+latency+"ms");
 	}
-}
-setTimeout(function () {
-	mc.on('data', ondata);
-}, 1000);
+});
+mc.on('login', function (username) {
+	m.say('#concerned', username+' is now playing Minecraft');
+});
+mc.on('logout', function (username, reason) {
+	m.say('#concerned', username+' quit Minecraft: '+reason);
+});
 m.notcommand('.', function (data) {
 	var line = data.from+": "+data.line;
-	swallowlines[line] = new Date;
-	mc.write('say '+line+'\n');
+	mc.say(line);
 });
 m.event(function (data) {
 	var line = "* "+data.line;
-	swallowlines[line] = new Date;
-	mc.write('say '+line+'\n');
+	mc.say(line);
 });
